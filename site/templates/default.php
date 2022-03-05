45f2ec9cf2878581c4f588a8ce8cd2728d054504
Wenzel Massag <wmassag@wenzels-macbook-pro.local>
2022-03-05 13:42:02 +0000

    Added Adobe/TyporKit Fonts

--- site/templates/default.php
+++ site/templates/default.php
@@ -3,6 +3,7 @@
 	<head>
 		<title><?= $page->title()->html() ?>, <?= $site->title()->html() ?></title>
 		<meta charset="UTF-8">
+		<link rel="stylesheet" href="https://use.typekit.net/vez5lsq.css">
 	</head>
 	<body>
 		<?= $page->title()->html() ?>
