c5db329818fb05ee17abbe4138b064eda278c0a6
Wenzel Massag <wmassag@wenzels-macbook-pro.local>
2022-03-05 14:17:58 +0000

    Added sample content for buttons and P

--- site/templates/default.php
+++ site/templates/default.php
@@ -4,8 +4,18 @@
 		<title><?= $page->title()->html() ?>, <?= $site->title()->html() ?></title>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="https://use.typekit.net/vez5lsq.css">
+		<?= css('assets/css/styles.css') ?>
 	</head>
 	<body>
 		<?= $page->title()->html() ?>
+		<div>
+			<p>Buttons</p>
+			<button>label</button>
+			<button class="error">error</button>
+			<div class="error"><button>error in div</button></div>
+			<button class="disabled">disabled</button>
+			<div class="disabled"><button>disabled in div</button></div>
+			<button disabled>disabled button</button>
+		</div>
 	</body>
 </html>
