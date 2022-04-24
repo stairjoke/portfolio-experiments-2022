fcabd7ea5df2e56faebd6c5f4e585c8a87ab44fd
Wenzel Massag <wmassag@wizard-of-ulron.local>
2022-04-24 19:38:05 +0000

    Page Header (Navigation)

---
+++ site/snippets/header.php
@@ -0,0 +1,24 @@
+<?php
+//Header Snippet
+?>
+<header>
+	<nav>
+		<ul>
+			<li class="optional-1">
+				<a id="logo" href=""><svg>
+						<use xlink:href="#logo"></use>
+					</svg><span>Home</span></a>
+			</li>
+			<li>
+				<a href="">🇩🇪<span class="optional-2"> Auf deutsch ansehen</span></a>
+			</li>
+		</ul>
+	</nav>
+	<nav class="flex-grow">
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">Projects</a></li>
+			<li><a href="">Blog</a></li>
+		</ul>
+	</nav>
+</header>
