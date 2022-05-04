d38943c86e3410ade0b01cedc9e6c859275b900f
Wenzel Massag <wmassag@wizard-of-ulron.local>
2022-05-04 20:14:56 +0000

    Moved H2 Organism to snippet with pubdate and breadcrumb nav

---
+++ site/snippets/o-h2.php
@@ -0,0 +1,44 @@
+<?php
+// Snippet – Organism H2
+?>
+
+<?php
+	if($page->headline()->exists()) {
+?>
+	<div class="o-h2">
+<?php
+		if($page->published()->exists()) {
+		//https://forum.getkirby.com/t/translating-date-formats/23435
+?>
+		<div class="pubdate">
+			<time datetime="<?= $page->published()->toDate('d.m.Y') ?>" pubdate="pubdate"><?= $page->published()->toDate('F l, Y') ?></time>
+		</div>
+<?php
+		} //endif page has pubdate
+?>
+		<h2><?= ($page->has('Headline')) ? $page->headline() : '' ?></h2>
+		<nav class="breadcrumb">
+			<ol>
+				<li>
+					<a href="">Blog</a>
+					<svg class="inline-icon _12-36">
+						<use xlink:href="#hierarchy-icon"></use>
+					</svg>
+				</li>
+				<li>
+					<ul class="inline-tags">
+						<li><a href="">#Tag</a></li>
+						<li><a href="">#Tag</a></li>
+						<li><a href="">#Tag</a></li>
+					</ul>
+					<svg class="inline-icon _12-36">
+						<use xlink:href="#hierarchy-icon"></use>
+					</svg>
+				</li>
+				<li>
+					<a>Current Page</a>
+				</li>
+			</ol>
+		</nav>
+	</div>
+<?php } //endif page has headline ?>
