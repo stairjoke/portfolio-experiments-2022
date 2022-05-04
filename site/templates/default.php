444ea8763478117e7844c402f7e5a8cc33758ad7
Wenzel Massag <wmassag@wizard-of-ulron.local>
2022-05-04 21:17:57 +0000

    Added fundamental layout to organism article-teaser and variable font-size to h3

--- site/templates/default.php
+++ site/templates/default.php
@@ -24,17 +24,19 @@
 				<article class="teaser">
 					<!-- !!! URL !!! -->
 					<img src="content/home/atomic-design-teaser.jpg" alt="Photo of the printed book 'Designing for Atomic Design'">
-					<h3>Designing for Atomic Design</h3>
-					<ul class="tag-list">
-						<li><a href="">Design Systems</a></li>
-						<li><a href="">Editorial & Print</a></li>
-						<li><a href="">Front-End Development</a></li>
-					</ul>
-					<section>
-						<p>In my Bachelor Thesis (Dec 2020), I re-interpreted the Atomic Design workflow by Brad Frost from a designer's perspective. In my thesis abstract I put it like this:</p>
-						<p>"This thesis deals with two mutually reinforcing problems: designing for a variable viewport size and design-development coordination."</p>
-					</section>
-					<a href="" class="button">view project</a>
+					<div class="column">
+						<h3>Designing for Atomic Design</h3>
+						<ul class="tag-list">
+							<li><a href="">Design Systems</a></li>
+							<li><a href="">Editorial & Print</a></li>
+							<li><a href="">Front-End Development</a></li>
+						</ul>
+						<section>
+							<p>In my Bachelor Thesis (Dec 2020), I re-interpreted the Atomic Design workflow by Brad Frost from a designer's perspective. In my thesis abstract I put it like this:</p>
+							<p>"This thesis deals with two mutually reinforcing problems: designing for a variable viewport size and design-development coordination."</p>
+						</section>
+						<a href="" class="button">view project</a>
+					</div>
 				</article>
 			</section>
 		</main>
