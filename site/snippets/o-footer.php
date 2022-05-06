944c59026e89c2a9ed8f3c917c2d3b1921f9a44a
Wenzel Massag <wmassag@wizard-of-ulron.local>
2022-05-06 13:17:28 +0000

    Updated footer-content and footer-content parsing

--- site/snippets/o-footer.php
+++ site/snippets/o-footer.php
@@ -2,5 +2,29 @@
 // Organism Footer
 ?>
 <footer>
-	Footer
+	<h2><?= $site->FooterTitle() ?></h2>
+	<div class="column">
+		<h3><?= $site->FooterColumn1Title() ?></h3>
+		<ul class="griddy">
+			<?php
+				$socials = explode(', ', htmlspecialchars($site->FooterSocials()->value()));
+				$pattern_square = '\[(.*?)\]';
+				$pattern_round  = "\((.*?)\)";
+				$pattern = "/".$pattern_square.$pattern_round."/";
+				
+				foreach($socials as $social) {	
+					preg_match($pattern, $social, $matches);
+					$words = $matches[1];
+					$url   = $matches[2];
+					echo("<li>Words: $words, URL: $url</li>");
+				}
+			?>
+		</ul>
+	</div>
+	<div class="column">
+		<h3><?= $site->FooterColumn2Title() ?></h3>
+	</div>
+	<div class="column">
+		<h3><?= $site->FooterColumn3Title() ?></h3>
+	</div>
 </footer>
