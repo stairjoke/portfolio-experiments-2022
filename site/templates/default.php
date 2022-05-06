799d71fd4a62a788f7fdf9c20392807d6c84533b
Wenzel Massag <wmassag@wizard-of-ulron.local>
2022-05-06 08:39:52 +0000

    Refactoring: Making ARTICLE more versatile and preparing it for more general content.

--- site/templates/default.php
+++ site/templates/default.php
@@ -22,22 +22,24 @@
 			<section>
 				<?php snippet('o-h2'); ?>
 				<article class="teaser">
-					<div class="graphic-column">
-						<!-- !!! URL !!! -->
-						<img src="content/home/atomic-design-teaser.jpg" alt="Photo of the printed book 'Designing for Atomic Design'">
-					</div>
-					<div class="text-column">
-						<h3>Designing for Atomic Design</h3>
-						<ul class="m-tag-list">
-							<li><a href="">Design Systems</a></li>
-							<li><a href="">Editorial & Print</a></li>
-							<li><a href="">Front-End Development</a></li>
-						</ul>
-						<section>
-							<p>In my Bachelor Thesis (Dec 2020), I re-interpreted the Atomic Design workflow by Brad Frost from a designer's perspective. In my thesis abstract I put it like this:</p>
-							<p>"This thesis deals with two mutually reinforcing problems: designing for a variable viewport size and design-development coordination."</p>
-						</section>
-						<a href="" class="button">view project</a>
+					<div class="block">
+						<div class="graphic-column">
+							<!-- !!! URL !!! -->
+							<img src="content/home/atomic-design-teaser.jpg" alt="Photo of the printed book 'Designing for Atomic Design'">
+						</div>
+						<div class="text-column">
+							<h3>Designing for Atomic Design</h3>
+							<ul class="m-tag-list">
+								<li><a href="">Design Systems</a></li>
+								<li><a href="">Editorial & Print</a></li>
+								<li><a href="">Front-End Development</a></li>
+							</ul>
+							<section>
+								<p>In my Bachelor Thesis (Dec 2020), I re-interpreted the Atomic Design workflow by Brad Frost from a designer's perspective. In my thesis abstract I put it like this:</p>
+								<p>"This thesis deals with two mutually reinforcing problems: designing for a variable viewport size and design-development coordination."</p>
+							</section>
+							<a href="" class="button">view project</a>
+						</div>
 					</div>
 				</article>
 			</section>
